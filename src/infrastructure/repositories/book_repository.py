e772912a79b0f5e7b7d717055ceb71a970fab229
vcjuliocesar <vcjuliocesar@outlook.com>
2023-11-14 19:28:48 -0600

    created book

--- src/infrastructure/repositories/book_repository.py
+++ src/infrastructure/repositories/book_repository.py
@@ -1,12 +1,15 @@
 from src.domain.models.book_entity import BookEntity as Book
 from src.infrastructure.repositories.interfaces.book_interface import BookInterface
+from src.infrastructure.configs.database import db_connect
 
 class BookRepository(BookInterface):
     
     def __init__(self) -> None:
         
-        self.collection = "books"
+        self.db = db_connect()
     
     def create(self, book: Book) -> Book:
+        
+        book.save()
         
         return book
