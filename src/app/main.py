5bfd569b210762978358adb61b8559851f347f79
vcjuliocesar <vcjuliocesar@outlook.com>
2023-12-01 16:47:20 -0600

    config enviroment

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from src.domain.models.base_entity import init
 from src.app.routers.api.book import book_router
+from src.infrastructure.configs.enviroment import get_enviroment_settinngs
+from src.infrastructure.exceptions.error_handler import ErrorHandler
 
+env = get_enviroment_settinngs()
 
 app = FastAPI()
+
+app.title = env().APP_NAME
+
+app.version = env().API_VERSION
+
+app.description = env().APP_DESCRIPTION
+
+app.add_middleware(ErrorHandler)
 
 app.include_router(book_router)
 
